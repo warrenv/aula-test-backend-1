05e6c0542415e96d93f854bece04665cb2c7fa29
warren vosper <straydogsw@gmail.com>
2018-12-19 15:09:17 -0600

    cleanup template code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import Koa from 'koa'
 
-import { middleware1, songs, song } from './middlewares'
+import { song, songs } from './middlewares'
 
 export default (localConfig = {}) => {
   const app = new Koa()
 
-  app.use(middleware1)
   app.use(songs)
   app.use(song)
-  app.use(ctx => { ctx.body = 'Hello Koa' })
 
   return app
 }
